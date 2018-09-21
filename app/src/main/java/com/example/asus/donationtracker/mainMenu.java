a39718809ebf571413184b5fff0a681de3328e37
MHromiak <mhromiak3@gatech.edu>
2018-09-21 18:35:18 -0400

    Correct info logs in, incorrect info crashes the app. Must fix

--- app/src/main/java/com/example/asus/donationtracker/mainMenu.java
+++ app/src/main/java/com/example/asus/donationtracker/mainMenu.java
@@ -1,11 +1,10 @@
 package com.example.asus.donationtracker;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class mainMenu extends AppCompatActivity {
 
@@ -13,18 +12,14 @@ public class mainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button logout = (Button) findViewById(R.id.logoutButton);
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent logOut = new Intent(mainMenu.this, login.class);
+                startActivity(logOut);
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
 }
--- app/src/main/java/com/example/asus/donationtracker/temp.java
+++ app/src/main/java/com/example/asus/donationtracker/temp.java
@@ -3,7 +3,6 @@ package com.example.asus.donationtracker;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -31,20 +30,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
  */
-public class login extends AppCompatActivity implements LoaderCallbacks<Cursor> {
-    /**
-     * Map of current users' e-mails and passwords"
-     */
-    private HashMap<String, String> userAccts;
+public class temp extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -71,12 +64,8 @@ public class login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        userAccts = new HashMap<>();
-        userAccts.put("donator3@yahoo.com", "password3");
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_temp);
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
@@ -92,8 +81,6 @@ public class login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
                 return false;
             }
         });
-
-
 
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
@@ -286,7 +273,7 @@ public class login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
     private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
         //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
         ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(login.this,
+                new ArrayAdapter<>(temp.this,
                         android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
 
         mEmailView.setAdapter(adapter);
@@ -328,8 +315,12 @@ public class login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
                 return false;
             }
 
-            if (userAccts.get(mEmail).equals(mPassword)) {
-                return true;
+            for (String credential : DUMMY_CREDENTIALS) {
+                String[] pieces = credential.split(":");
+                if (pieces[0].equals(mEmail)) {
+                    // Account exists, return true if the password matches.
+                    return pieces[1].equals(mPassword);
+                }
             }
 
             // TODO: register the new account here.
@@ -343,8 +334,6 @@ public class login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
 
             if (success) {
                 finish();
-                Intent toMainMenu =  new Intent(login.this, mainMenu.class);
-                startActivity(toMainMenu);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();

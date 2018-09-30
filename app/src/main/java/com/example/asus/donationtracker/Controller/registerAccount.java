5baf7d7d9148856dc21bcb8754a4c6e52765a4d4
MHromiak <mhromiak3@gatech.edu>
2018-09-30 18:35:24 -0400

    Work on new register account activity

--- app/src/main/java/com/example/asus/donationtracker/Controller/registerAccount.java
+++ app/src/main/java/com/example/asus/donationtracker/Controller/registerAccount.java
@@ -1,14 +1,7 @@
 package com.example.asus.donationtracker.Controller;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
-
-import java.util.HashMap;
+import android.os.Bundle;
 
 import com.example.asus.donationtracker.R;
 
@@ -18,18 +11,5 @@ public class registerAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_account);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }

9a8bde7feaa9c831b3f1f25e4d4c9e44117d5d77
Mike Lewis <mike@mplew.is>
2018-11-04 23:19:07 -0500

    Refactor Location class

--- app/src/main/java/com/example/asus/donationtracker/Model/Location.java
+++ app/src/main/java/com/example/asus/donationtracker/Model/Location.java
@@ -1,111 +1,130 @@
 package com.example.asus.donationtracker.Model;
 
+
 import android.util.Log;
 
 import java.io.Serializable;
 
+
 public class Location implements Serializable {
-
-    /** Location's name**/
-    private String name;
-
-    /** Location's type**/
-    private LocationType locationType;
-
-    /** Location's longitude**/
-    private double longitude;
-
-    /** Location's latitude**/
-    private double latitude;
-
-    /** Location's address**/
-    private String address;
-
-    /** Location's city**/
-    private String city;
-
-    /** Location's state**/
-    private String state;
-
-    /** Location's ZIP**/
-    private String zip;
-
-    /** Location's phone number**/
-    private String phoneNumber;
-
-    /**
-     * creates a new location
-     */
-    public Location(String name, LocationType locationType, double longitude, double latitude,
-                    String address, String city, String state, String zip, String phoneNumber) {
-        this.name = name;
-        this.locationType = locationType;
-        this.longitude = longitude;
-        this.latitude = latitude;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (!(o instanceof Location)) {
-            return false;
-        }
-
-        return (((Location) o).getName().equals(this.name)
-                && ((Location) o).getLocationType() == this.locationType
-                && (((Location) o).getLongitude() == this.longitude))
-                && ((Location) o).getLatitude() == (this.latitude)
-                && ((Location) o).getAddress().equals(this.address)
-                && ((Location) o).getCity().equals(this.city)
-                && ((Location) o).getState().equals(this.state)
-                && ((Location) o).getZip().equals(this.zip)
-                && ((Location) o).getPhoneNumber().equals(this.phoneNumber);
-    }
-
-
-    /* *************************************
-     * All property getters and setters
-     */
-
-    public String getName() {return name;}
-    public void setName(String call) {name = call;}
-
-    public LocationType getLocationType() {return locationType;}
-    public void setLocationType(LocationType type) {locationType = type;}
-
-    public double getLongitude() {return longitude;}
-    public void setLongitude(double longy) {longitude = longy;}
-
-    public double getLatitude() {return latitude;}
-    public void setLatitude(double laty) {latitude = laty;}
-
-    public String getAddress() {return address;}
-    public void setAddress(String place) {address = place;}
-
-    public String getPhoneNumber() {return phoneNumber;}
-    public void setPhoneNumber(String num) {phoneNumber = num;}
-
-    public String getCity() {return city;}
-    public void setCity(String c) {city = c;}
-
-    public String getState() {return state;}
-    public void setState(String s) {state = s;}
-
-    public String getZip() {return zip;}
-    public void setZip(String z) {zip = z;}
-
-    /**********************************************/
-
-
-    @Override
-    public String toString() {
-        return (name + ", " + address + " : " + longitude + "," + latitude + " : " + phoneNumber);
-    }
+	
+	private String name;
+	
+	private LocationType locationType;
+	
+	private double longitude;
+	private double latitude;
+	
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	
+	private String phoneNumber;
+	
+	
+	public Location(String name, LocationType locationType, double longitude, double latitude, String address, String city, String state, String zip, String phoneNumber) {
+		this.name = name;
+		this.locationType = locationType;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (other == this) {
+			return true;
+		}
+		
+		if (!(other instanceof Location)) {
+			return false;
+		}
+		
+		Location that = (Location) other;
+		
+		return that.getName().equals(this.name)
+				&& (that.getLocationType() == this.locationType)
+				&& (that.getLongitude() == this.longitude)
+				&& (that.getLatitude() == this.latitude)
+				&& that.getAddress().equals(this.address)
+				&& that.getCity().equals(this.city)
+				&& that.getState().equals(this.state)
+				&& that.getZip().equals(this.zip)
+				&& that.getPhoneNumber().equals(this.phoneNumber);
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public LocationType getLocationType() {
+		return locationType;
+	}
+	public void setLocationType(LocationType locationType) {
+		this.locationType = locationType;
+	}
+	
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return (name + ", " + address + " : " + longitude + "," + latitude + " : " + phoneNumber);
+	}
 }

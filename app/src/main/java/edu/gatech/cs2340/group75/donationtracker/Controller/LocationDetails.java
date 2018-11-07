b172206097206906c22249971672cb8333d1cd51
Mike Lewis <mike@mplew.is>
2018-11-07 14:14:29 -0500

    Fully rename class

--- app/src/main/java/edu/gatech/cs2340/group75/donationtracker/Controller/LocationDetails.java
+++ app/src/main/java/edu/gatech/cs2340/group75/donationtracker/Controller/LocationDetails.java
@@ -85,7 +85,7 @@ public class LocationDetails extends AppCompatActivity {
         donateFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    Intent intent = new Intent(LocationDetails.this, enterDonationItem.class);
+                    Intent intent = new Intent(LocationDetails.this, EnterDonationItem.class);
                     intent.putExtra("LOCATION", location);
                     startActivity(intent);
 

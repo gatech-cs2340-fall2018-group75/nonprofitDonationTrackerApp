3a4ae3252450bf577719056289d9b70c3ccb02f5
Mike Lewis <mike@mplew.is>
2018-11-11 19:27:02 -0500

    Use put instead of replace

--- app/src/main/java/edu/gatech/cs2340/group75/donationtracker/controller/ActivityClasses.java
+++ app/src/main/java/edu/gatech/cs2340/group75/donationtracker/controller/ActivityClasses.java
@@ -3,12 +3,14 @@ package edu.gatech.cs2340.group75.donationtracker.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.util.Log;
+
 class ActivityClasses {
 	
 	private static final Map<String, Class> classes = new HashMap<>();
 	
 	public void add(Class newClass) {
-		classes.replace(newClass.getSimpleName(), newClass);
+		classes.put(newClass.getSimpleName(), newClass);
 	}
 	
 	public Class get(String name) {
